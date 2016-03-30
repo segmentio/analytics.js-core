a657f6cad9de5c42f1bbbe92624f5c439f70f613
Sam Levan <sam@madkudu.com>
2016-03-30 09:56:46 -0700

    update and add tests on predictive function

--- test/predictions.js
+++ test/predictions.js
@@ -2,6 +2,10 @@ var assert = require('assert');
 var analytics = window.analytics;
 
 describe('predictions', function() {
+
+  var user_id = 'my_user_id';
+
+  // good customer
   it('case 1: should return very good for this one ', function() {
     var traits = {
       employees: 200,
@@ -9,11 +13,12 @@ describe('predictions', function() {
       raised: 0,
       industry: 'Software'
     };
-    analytics.identify(traits);
+    analytics.identify(user_id, traits);
     var score = analytics.user().predictions();
     assert.deepEqual(score, { mk_customer_fit: 'very good' });
   });
 
+  // customer too small
   it('case 2: should be undefined', function() {
     var traits = {
       employees: 30,
@@ -21,11 +26,12 @@ describe('predictions', function() {
       raised: 0,
       industry: 'Software'
     };
-    analytics.identify(traits);
+    analytics.identify(user_id, traits);
     var score = analytics.user().predictions();
-    assert.deepEqual(score, { mk_customer_fit: undefined });
+    assert.deepEqual(score, { mk_customer_fit: 'not very good' });
   });
 
+  // industry value is not one of the allowed value
   it('case 3: should be undefined', function() {
     var traits = {
       employees: 3000,
@@ -33,11 +39,12 @@ describe('predictions', function() {
       raised: 0,
       industry: 'Something Strange'
     };
-    analytics.identify(traits);
+    analytics.identify(user_id, traits);
     var score = analytics.user().predictions();
     assert.deepEqual(score, { mk_customer_fit: undefined });
   });
 
+  // good customer, has raised LOTS of $
   it('case 4: should be very good', function() {
     var traits = {
       employees: 60,
@@ -45,11 +52,12 @@ describe('predictions', function() {
       raised: 30000000000,
       industry: 'Commercial Services & Supplies'
     };
-    analytics.identify(traits);
+    analytics.identify(user_id, traits);
     var score = analytics.user().predictions();
     assert.deepEqual(score, { mk_customer_fit: 'very good' });
   });
 
+  // small and not a great industry
   it('case 5: should be undefined', function() {
     var traits = {
       employees: 60,
@@ -57,8 +65,55 @@ describe('predictions', function() {
       raised: 300,
       industry: 'Commercial Services & Supplies'
     };
-    analytics.identify(traits);
+    analytics.identify(user_id, traits);
+    var score = analytics.user().predictions();
+    assert.deepEqual(score, { mk_customer_fit: 'not very good' });
+  });
+
+  // User with an unexpected trait
+  it('case 6: should return very good', function() {
+    var traits = {
+      employees: 200,
+      alexaGlobalRank: 3000,
+      raised: 0,
+      industry: 'Software',
+      number_of_arms: 7
+    };
+    analytics.identify(user_id, traits);
+    var score = analytics.user().predictions();
+    assert.deepEqual(score, { mk_customer_fit: 'very good' });
+  });
+
+  // User with a trait in a different format
+  it('case 7: should be undefined', function() {
+    var traits = {
+      employees: '200',
+      alexaGlobalRank: 3000,
+      raised: 0,
+      industry: 'Software'
+    };
+    analytics.identify(user_id, traits);
     var score = analytics.user().predictions();
     assert.deepEqual(score, { mk_customer_fit: undefined });
   });
+
+  // User with a missing trait
+  it('case 8: should be undefined', function() {
+    var traits = {
+      employees: '200',
+      alexaGlobalRank: 3000,
+      raised: 0
+    };
+    analytics.identify(user_id, traits);
+    var score = analytics.user().predictions();
+    assert.deepEqual(score, { mk_customer_fit: undefined });
+  });
+
+  // User with traits as undefined
+  it('case 9: should be undefined', function() {
+    analytics.identify(user_id);
+    var score = analytics.user().predictions();
+    assert.deepEqual(score, { mk_customer_fit: undefined });
+  });
+
 });
