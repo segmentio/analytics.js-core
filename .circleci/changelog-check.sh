e434971bafcdfdc512ddb85062cb2333e7a1ae16
Daniel Jackins <danieljackins@gmail.com>
2020-02-24 20:23:50 -0800

    hopefully fix, require changelog for publish

--- .circleci/changelog-check.sh
+++ .circleci/changelog-check.sh
@@ -9,11 +9,11 @@ SITE_CHANGES=$(git diff "$BRANCH".."$REMOTE" | wc -l)
 echo "Detected $SITE_CHANGES changes"
 if [ "$SITE_CHANGES" -gt "0" ]; then
   echo "Checking to make sure package version was updated..."
-  VERSION_CHANGED=$(git diff "$BRANCH".."$REMOTE" -G '"version":' -- $REPO_ROOT/package.json | wc -l)
-  if [ "$VERSION_CHANGED" -gt "0" ]; then
-    echo "Version was updated!  Continuing..."
+  HISTORY_CHANGED=$(git diff "$BRANCH".."$REMOTE" -- $REPO_ROOT/HISTORY.md | wc -l)
+  if [ "$HISTORY_CHANGED" -gt "0" ]; then
+    echo "History was updated!  Continuing..."
   else
-    echo "Version was not updated :( Aborting push."
+    echo "History was not updated :( Aborting push."
     exit 1
   fi
 fi
