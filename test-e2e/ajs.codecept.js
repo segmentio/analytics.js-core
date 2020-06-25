3459e110052779be1c78df8e6ece4636af51669a
King Long Tse <kinglong.tse@segment.com>
2020-06-25 14:15:21 -0700

    added login/logout test

--- test-e2e/ajs.codecept.js
+++ test-e2e/ajs.codecept.js
@@ -24,3 +24,36 @@ Scenario(
     assert.equal(lsUserId, `"fathy"`);
   }
 ).injectDependencies({ testID: 'cookies-and-local-storage' });
+
+Scenario('Login as a different user', async (I, testID) => {
+  I.loadAJS({ local: true });
+
+  I.startRecording(testID);
+
+  I.click('#identify-spongebob');
+  let userId = await I.grabCookie('ajs_user_id');
+  assert.equal(userId.value, '%22spongebob%22');
+  let lsUserId = await I.executeScript(() => {
+    return localStorage.getItem('ajs_user_id');
+  });
+  assert.equal(lsUserId, `"spongebob"`);
+  I.click('#track-product-viewed');
+
+  I.click('#identify-fathy');
+  userId = await I.grabCookie('ajs_user_id');
+  assert.equal(userId.value, '%22fathy%22');
+  lsUserId = await I.executeScript(() => {
+    return localStorage.getItem('ajs_user_id');
+  });
+  assert.equal(lsUserId, `"fathy"`);
+  I.click('#track-product-viewed');
+
+  I.click('#reset');
+  userId = await I.grabCookie('ajs_user_id');
+  assert.strictEqual(userId, undefined);
+  lsUserId = await I.executeScript(() => {
+    return localStorage.getItem('ajs_user_id');
+  });
+  assert.strictEqual(lsUserId, null);
+  await I.stopRecording(testID);
+}).injectDependencies({ testID: 'login-as-different-user' });
